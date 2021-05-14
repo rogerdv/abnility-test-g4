b7e582f268b0438d781b7ee4e883250524a70e5f
rogerdv <rogerdv@gmail.com>
2021-05-13 21:29:23 -0400

    Little fixes

--- scenes/scene_test.gd
+++ scenes/scene_test.gd
@@ -1,11 +1,6 @@
 extends Node3D
 
-var LEFT:bool=false
-var RIGHT:bool=false
-var FORWARD:bool=false
-var BACK:bool=false
 var joystick
-var ROTATE:int=0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
